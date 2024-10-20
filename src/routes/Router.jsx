b56ad3af88c52d007fbce6091a084ce1c4b21910
Md. Sabbir Al Mamon <md.sabbiralmamon@gmail.com>
2024-10-20 13:45:40 +0600

    refactor: Update Router component to use Root and Home components
    This commit updates the Router component in the src/routes/Router.jsx file. It replaces the previous static content with the Root and Home components. The Root component is used as the layout for the application, while the Home component is rendered as the main content for the "/" path. This change improves the modularity and maintainability of the code.

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,9 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
+import Home from "../pages/Home/Home";
+import Root from "../layouts/Root/Root";
 
 const Router = createBrowserRouter([
     {
       path: "/",
-      element: <div>Hello world!</div>,
+      element: <Root />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        }
+      ],
     },
   ]);
 
