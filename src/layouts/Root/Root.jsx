43e532eb4afb61c1354e48cf446378bdb7f334e7
Md. Sabbir Al Mamon <md.sabbiralmamon@gmail.com>
2024-10-21 16:13:07 +0600

    feat: Add Navbar component to Root layout

--- src/layouts/Root/Root.jsx
+++ src/layouts/Root/Root.jsx
@@ -1,9 +1,12 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../../components/shared/Navbar/Navbar";
 
 const Root = () => {
     return (
         <div>
-            <header> header </header>
+            <header> 
+                <Navbar />
+             </header>
             <Outlet />
             <footer> footer </footer>
         </div>
